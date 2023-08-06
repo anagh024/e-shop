2353eb79e3e882f9122df5b4e9e6a6409eedde8d
Msuf123 <jack@gmil.com>
2023-08-06 22:59:23 +0530

    started seach options in modile mode

--- src/Components/Shop/mobileoption/mobileoption.js
+++ src/Components/Shop/mobileoption/mobileoption.js
@@ -4,12 +4,15 @@ import Option from "../Filter/Input/Option";
 import style from './mobile.module.css'
 import Input from '../Filter/Input/Input';
 import Slider from '../Filter/Slider/Slider';
-export default function MobileOptions(){
+export default function MobileOptions({show,setShow}){
     let category=useSelector((state)=>(state.filter.category))
-    console.log(category)
+    
     category=Object.keys(category)
     return(
         <div className={style.main}>
+            <button onClick={()=>{
+setShow(false)
+            }}>&#10060;</button>
         <Input></Input>
          <div>
          {category.map((i,j)=>(<Option name={i} key={j}></Option>))}
--- src/Components/Shop/moblie.js
+++ src/Components/Shop/moblie.js
@@ -2,17 +2,24 @@ import { useNavigate } from 'react-router-dom'
 import src from '../../pngwing.com.png'
 import src_t from '../../pngwing1.com.png'
 import MobileOptions from './mobileoption/mobileoption'
+import { useState } from 'react'
 export default function Mobile(){
     const nav=useNavigate()
+    const[show,setShow]=useState(false)
     return(
         <div className="mobile-filter">
             
-           <button className='border-color'><i class="fa fa-filter"style={{fontSize:'40px'}}></i>
+           <button className='border-color'
+           onClick={()=>{
+            setShow(true)
+           }}
+           ><i class="fa fa-filter"style={{fontSize:'40px'}}></i>
            
            </button> 
+           {show?
            <div className='search-bar-mobile'>
-               <MobileOptions></MobileOptions>
-           </div>
+               <MobileOptions show={show} setShow={setShow} ></MobileOptions>
+           </div>:null}
             <button><i class="fa fa-shopping-cart" style={{fontSize:'40px'}} onClick={()=>{
                 nav('/cart')
             }}></i></button>
